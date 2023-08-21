74a6604d84fb0dfe1b18b4eac215fb83ecf621d4
arpitdobariya <100218089+arpitdobariya@users.noreply.github.com>
2023-08-21 13:46:03 +0530

    Add files via upload

---
+++ model/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    upvotedQuestions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }],
+    downvotedQuestions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }],
+    comments: [
+      {
+        question: { type: mongoose.Schema.Types.ObjectId, ref: 'Question' },
+        content: { type: String, required: true },
+        createdAt: { type: Date, default: Date.now },
+      },
+    ],
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

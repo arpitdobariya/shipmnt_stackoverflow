0a2ed781df1aae38cde3ed9b636eb545450e1513
arpitdobariya <100218089+arpitdobariya@users.noreply.github.com>
2023-08-21 13:45:25 +0530

    Create questions.js

---
+++ model/questions.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  upvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  downvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  comments: [
+    {
+      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      content: { type: String, required: true },
+      createdAt: { type: Date, default: Date.now },
+    },
+  ],
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const Question = mongoose.model('Question', questionSchema);
+
+module.exports = Question;
